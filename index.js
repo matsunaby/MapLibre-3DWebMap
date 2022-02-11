fd5f61fb847ea8bccb4e7c1cac0f6c0e1853e666
hinamei <hina.gntm@gmail.com>
2022-02-11 15:08:33 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const map = new maplibregl.Map({
+    container: 'map',
+    style: 'style.json',
+    center: [139.767476, 35.680773],
+    zoom: 17,
+    hash: true,
+    pitch: 30,
+    localIdeographFontFamily: false
+})
