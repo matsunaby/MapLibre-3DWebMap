d31ae5b2e3d913560987ece03e616eb01858c6d3
hinamei <hina.gntm@gmail.com>
2022-02-11 16:20:28 +0900

    add sample GeoJSON polygon layer

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-const map = new maplibregl.Map({
+// ベースマップを作成する
+// ここでは3DのOpenStreetMapを表示する
+var map =  new maplibregl.Map({
     container: 'map',
     style: 'style.json',
-    center: [139.767476, 35.680773],
-    zoom: 17,
+    center: [129.768337, 32.986804],
+    zoom: 19,
     hash: true,
     pitch: 30,
     localIdeographFontFamily: false
 })
+
+map.on('load', function () {
+    map.addSource('sample', {
+        type: 'geojson',
+        data: './data/nagasaki_polygon.geojson'
+    });
+
+    // スタイルを設定
+    map.addLayer({
+        'id': 'sample',
+        'type': 'fill',
+        'source': 'sample',
+        'layout': {},
+        'paint': {
+        'fill-color': '#088',
+        'fill-opacity': 0.8
+        }      
+    });
+});
+
+/* // GeoJSON を読み込みレイヤを表示する
+function addGeoJSONLayer(map){
+    // データソースを定義
+    map.addSource('sample', {
+        type: 'geojson',
+        data: './data/nagasaki_polygon.geojson'
+    });
+
+    // スタイルを設定
+    map.addLayer({
+        'id': 'sample',
+        'type': 'fill',
+        'source': 'sample',
+        'layout': {},
+        'paint': {
+        'fill-color': '#088',
+        'fill-opacity': 0.8
+        }      
+    });
+} */
