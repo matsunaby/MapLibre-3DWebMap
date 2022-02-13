d1920361aff9ac9c94604fa3ae1126a780b9d2ba
matsunaby <99102996+matsunaby@users.noreply.github.com>
2022-02-13 14:09:26 +0900

    急傾斜地崩壊危険区域と土砂災害警戒危険区域を地図に反映したものですが、土砂災害警戒危険区域の塗り分けはできていません。

--- index.js
+++ index.js
@@ -11,6 +11,40 @@ var map =  new maplibregl.Map({
 })
 
 map.on('load', function () {
+    map.addSource('dosya-keikai', {
+        type: 'geojson',
+        data: './data/nagasaki_dosya-keikai.geojson'
+    });
+
+    // スタイルを設定
+    map.addLayer({
+        'id': 'dosya-keikai',
+        'type': 'fill',
+        'source': 'dosya-keikai',
+        'layout': {},
+        'paint': {
+            'fill-color': 'Yellow',
+            'fill-opacity': 0.5
+        }   
+    });
+
+    map.addSource('kyukei-kiken', {
+        type: 'geojson',
+        data: './data/nagasaki_kyukei-kiken.geojson'
+    });
+
+    // スタイルを設定
+    map.addLayer({
+        'id': 'kyukei-kiken',
+        'type': 'fill',
+        'source': 'kyukei-kiken',
+        'layout': {},
+        'paint': {
+            'fill-color': 'Red',
+            'fill-opacity': 0.5
+        }   
+    });
+
     map.addSource('shelter_point', {
         type: 'geojson',
         data: './data/nagasaki_shelter.geojson'
@@ -23,7 +57,7 @@ map.on('load', function () {
         'source': 'shelter_point',
         'layout': {},
         'paint': {
-            'circle-color': '#FF0000',
+            'circle-color': 'Green',
             'circle-radius': 5
         }   
     });
